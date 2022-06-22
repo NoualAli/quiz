3bd7b95f7df1fb385c93a99be77b67dddfe2c90c
Ali Noual <noualdev@gmail.com>
2022-06-22 20:53:12 +0100

    Ajout de la meta csrf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>Quiz</title>
 </head>
 
