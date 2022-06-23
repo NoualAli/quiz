239364be821ba7e1d88de92fdbdfe558d93d10cb
Ali Noual <noualdev@gmail.com>
2022-06-23 17:10:27 +0100

    Création de la table quiz_entries

---
+++ database/migrations/2022_06_22_205553_create_quiz_entries_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quiz_entries', function (Blueprint $table) {
+            $table->id();
+            $table->string('participent', 50)->unique();
+            $table->json('answers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quiz');
+    }
+};
